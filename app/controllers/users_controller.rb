a4396ca6060d861b3718b65252d00f7d60da7674
vagrant <nori.work.vtr250@gmail.com>
2020-07-27 04:14:03 +0000

    add ユーザー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
-  	
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :profile_image, :introduction)
+  	end
 end
